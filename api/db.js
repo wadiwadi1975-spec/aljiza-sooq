d46756f94d3f24d4b2470c5c63b91a55ff11b6c6
wadiwadi1975-spec <wadi.wadi1975@gmail.com>
2026-09-10 17:56:16 +0300

    Fix SSL config

--- api/db.js
+++ api/db.js
@@ -5,7 +5,7 @@ const { Pool } = require('pg');
 // ---- PostgreSQL connection ----
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }
+  ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false
 });
 
 // ---- crypto helpers ----
